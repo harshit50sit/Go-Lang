3fe461ece9d72d00134d290873b54f1df228b4a8
harshit50sit <harshitverma26@gmail.com>
2021-03-07 21:04:23 +0530

    forLoops

--- Go-Lang/cards/main.go
+++ Go-Lang/cards/main.go
@@ -15,6 +15,19 @@ func main() {
 	//function declaration
 	newcard := newCard()
 	fmt.Println(newcard)
+	//Slices & for loops
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades") //add a new element
+	fmt.Println(cards)
+	//Iterate over slice of cards
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	//2nd metode
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+	}
 }
 
 func newCard() string {
