32aec9f45552d081267882c97f00a74053b4e166
harshit50sit <harshitverma26@gmail.com>
2021-03-07 20:33:28 +0530

    variable_basic

--- Go-Lang/cards/main.go
+++ Go-Lang/cards/main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+var deckSize int
+
 func main() {
+	//var card string = "Ace of Spades"
+	//or
+	deckSize = 20
+	card := "Ace of Spades"
+	card = "five of Diamonds"
+	fmt.Println(card)
+	fmt.Println(deckSize)
 
 }
